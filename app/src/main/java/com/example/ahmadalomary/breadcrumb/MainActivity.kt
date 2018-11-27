afb54940ed37e85b7fd0e5fffdd74c5d04b0a6e1
ahmad.alomary <ahmad.alomary@autotrader.co.uk>
2018-11-27 16:25:16 +0000

    breadcrumb 1-5

--- app/src/main/java/com/example/ahmadalomary/breadcrumb/MainActivity.kt
+++ app/src/main/java/com/example/ahmadalomary/breadcrumb/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.ahmadalomary.breadcrumb
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.motion.MotionLayout
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.breadcrumb.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        crumb1.setOnClickListener { run() }
+        crumb2.setOnClickListener { run2() }
+        crumb3.setOnClickListener { run3() }
+        crumb4.setOnClickListener { run4() }
+        crumb5.setOnClickListener { run5() }
+    }
+
+    fun run() {
+        (bread as MotionLayout).transitionToState(R.id.animate1_2)
+
+    }
+
+    fun run2() {
+        (bread as MotionLayout).transitionToState(R.id.animate2_3)
+
+    }
+
+    fun run3() {
+        (bread as MotionLayout).transitionToState(R.id.animate3_4)
+
+    }
+
+    fun run4() {
+        (bread as MotionLayout).transitionToState(R.id.animate4_5)
+
+    }
+
+    fun run5() {
+        (bread as MotionLayout).transitionToState(R.id.animate5_1)
+
     }
 }

c2bfb5d0ec3f072dbab66b045a4db64fee2d7e06
ahmad.alomary <ahmad.alomary@carzone.ie>
2019-05-16 09:36:41 +0100

    something works

--- app/src/main/java/com/example/ahmadalomary/breadcrumb/MainActivity.kt
+++ app/src/main/java/com/example/ahmadalomary/breadcrumb/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.support.constraint.motion.MotionLayout
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.breadcrumb.*
+import kotlinx.android.synthetic.main.breadcrumb_alpha2.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,31 +15,30 @@ class MainActivity : AppCompatActivity() {
         crumb2.setOnClickListener { run2() }
         crumb3.setOnClickListener { run3() }
         crumb4.setOnClickListener { run4() }
-        crumb5.setOnClickListener { run5() }
+//        crumb5.setOnClickListener { run5() }
     }
 
     fun run() {
-        (bread as MotionLayout).transitionToState(R.id.animate1_2)
-
+//        (bread as MotionLayout).transitionToState(R.id.animate1_2)
+        (bread as MotionLayout).transitionToState(R.id.one_two)
     }
 
     fun run2() {
-        (bread as MotionLayout).transitionToState(R.id.animate2_3)
-
+//        (bread as MotionLayout).transitionToState(R.id.animate2_1)
+        (bread as MotionLayout).transitionToState(R.id.three_two)
     }
 
     fun run3() {
-        (bread as MotionLayout).transitionToState(R.id.animate3_4)
-
+//        (bread as MotionLayout).transitionToState(R.id.animate2_3)
+        (bread as MotionLayout).transitionToState(R.id.two_three)
     }
 
     fun run4() {
-        (bread as MotionLayout).transitionToState(R.id.animate4_5)
-
+//        (bread as MotionLayout).transitionToState(R.id.animate3_2)
+        (bread as MotionLayout).transitionToState(R.id.three_one)
     }
 
     fun run5() {
-        (bread as MotionLayout).transitionToState(R.id.animate5_1)
-
+//        (bread as MotionLayout).transitionToState(R.id.animate5_1)
     }
 }
